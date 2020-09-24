1659cfd4f00ecb8289fda62db4d6f1c802aa7535
JeremyPK <jpkeeton@gmail.com>
2020-09-24 08:56:33 -0700

    adding some xlwings notes
    sorta working...

---
+++ firstPythonXL.py
@@ -0,0 +1,17 @@
+import xlwings as xw
+
+
+@xw.sub  # only required if you want to import it or run it via UDF Server
+def main():
+    wb = xw.Book.caller()
+    wb.sheets[0].range("A1").value = "Hello xlwings!"
+
+
+@xw.func
+def hello(name):
+    return "hello {0}".format(name)
+
+
+if __name__ == "__main__":
+    xw.books.active.set_mock_caller()
+    main()
